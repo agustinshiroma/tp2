5ef15777d62b3a13844d995665d12fcd7f5c5b20
pablof@tecnoshare.com <pablof@tecnoshare.com>
2022-03-27 22:50:23 -0300

    pre clase 3

---
+++ demos/basic/arrays-adv.js
@@ -0,0 +1,42 @@
+const inventors = [
+    { first: 'Albert', last: 'Einstein', year: 1879 },
+    { first: 'Isaac', last: 'Newton', year: 1643 },
+    { first: 'Galileo', last: 'Galilei', year: 1564 },
+    { first: 'Marie', last: 'Curie', year: 1867 },
+    { first: 'Johannes', last: 'Kepler', year: 1571 },
+    { first: 'Nicolaus', last: 'Copernicus', year: 1473 },
+    { first: 'Max', last: 'Planck', year: 1858 },
+  ];
+
+  // TODO 1: Función para mostrar por consola en formato tabular
+
+  // TODO 2: Mostrar por consola los inventores nacidos antes del 1800
+
+  // TODO 3: Mostrar por consola los inventores cuyo apellido empiece con C
+
+  // TODO 4: Mostrar por consola Convertir el apellido en mayusculas
+
+  // TODO 5: Mostrar aplicando 3 y 4
+
+  // TODO 6: Buscar el inventor Kepler y retornar el objeto
+
+  // TODO 7: Hay algun inventor nacido en 1858
+
+  // TODO 8: Todos los inventores nacieron despues de 1500
+
+  // TODO 9: Mostrar por consola los inventores ordenados ascendetemente por fecha de nacimiento
+
+  // TODO 10: Ordenar los inventores por apellido
+
+  // TODO 11: Converir el array al formato:
+  /**
+     * [{name: 
+     *      {
+     *          first: 'first name',
+     *          last: 'last name'
+     *      }
+     * }, 
+     * year: year
+     * ]
+     * 
+  */ 
